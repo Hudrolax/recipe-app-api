f389f117987e18e21f973948615f389fe7804551
Sergey <hudro@MacBook-hudro.local>
2022-11-16 17:57:55 +0400

    sc

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,19 @@
-from django.db import models # noqa
+"""
+Datebase models.
+"""
 
-# Create your models here.
+from enum import unique
+from django.db import models
+from django.contrib.auth import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermisionsMixin,
+)
+
+
+class User(AbstractBaseUser, PermisionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
